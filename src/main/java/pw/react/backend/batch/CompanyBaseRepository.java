1ff77d8fe85e10bc0d18679cd2ec422558981341
PreCyz <precpaw@op.pl>
2022-12-27 22:35:16 +0100

    Fixing UTC.

--- src/main/java/pw/react/backend/batch/CompanyBaseRepository.java
+++ src/main/java/pw/react/backend/batch/CompanyBaseRepository.java
@@ -1,13 +1,14 @@
 package pw.react.backend.batch;
 
-import org.jooq.*;
+import org.jooq.DSLContext;
+import org.jooq.Query;
 import org.jooq.impl.DSL;
 import org.springframework.transaction.annotation.Transactional;
 import pw.react.backend.models.Company;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.util.Collection;
+import java.util.Optional;
 
 import static pw.react.backend.models.backend.Tables.COMPANY;
 
@@ -23,20 +24,24 @@ class CompanyBaseRepository implements BatchRepository<Company> {
     public Collection<Company> insertAll(Collection<Company> entities) {
         Query[] queries = new Query[entities.size()];
         int idx = 0;
-        Record1<Long> maxId = dsl.select(DSL.max(COMPANY.ID)).from(COMPANY).fetch().get(0);
+        Long maxId = dsl.select(DSL.max(COMPANY.ID)).from(COMPANY).fetch().get(0).value1();
         for (Company company : entities) {
+            LocalDateTime date = Optional.ofNullable(company.getStartDateTime())
+                    .orElseGet(() -> Instant.now().atZone(ZoneId.systemDefault()).toLocalDateTime());
+
             queries[idx++] = dsl.insertInto(COMPANY,
                     COMPANY.ID,
                     COMPANY.BOARD_MEMBERS,
                     COMPANY.NAME,
                     COMPANY.START_DATE
             ).values(
-                    maxId.value1() + idx,
+                    maxId + idx,
                     company.getBoardMembers(),
                     company.getName(),
-                    ZonedDateTime.of(company.getStartDateTime(), ZoneId.of("UTC")).toLocalDateTime()
+                    date
             );
-            company.setId(maxId.value1() + idx);
+            company.setId(maxId + idx);
+            company.setStartDateTime(date);
         }
         dsl.batch(queries).execute();
         return entities;
